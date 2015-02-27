b4ce04ebf119fe831f0c96c2b9daf516351c23a7
Nikolas Grottendieck <github@nikolasgrottendieck.com>
2015-02-27 14:13:15 +0100

    Examples and documentation updated

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 yii2-chartist
 =============
-A Yii2 extension for [chartist.js](http://gionkunz.github.io/chartist-js/index.html).
+A Yii2 extension for [Chartist.js](http://gionkunz.github.io/chartist-js/index.html).
 
 ## Installation
 
@@ -21,31 +21,30 @@ to the ```require``` section of your `composer.json` file.
 
 ## Usage
 
-```php
-use okeanos\chartist\Chartist;
+You can use the ```htmlOptions``` to manually set an ID and subsequently used that to refer to the specific Chartist.js instance. Check the examples in `examples.php` to see how the [Chartist.js examples](http://gionkunz.github.io/chartist-js/examples.html) work with this extension.
 
-echo Chartist::widget([
-	'tagName' => 'div', // HTML-container for the chart, optional parameter
-	'data' => [
-		'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-		'series' => [ 
-			[5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
-            [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
-        ]
-	],
+### Source
+
+```php
+<?php
+use okeanos\chartist\Chartist;
+use yii\helpers\Json;
+use yii\web\JsExpression;
+?>
+<?= Chartist::widget([
+	'tagName' => 'div',
+	'data' => new JsExpression(Json::encode([
+            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ],
+            'series' => [
+                [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
+                [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
+            ]
+        ])),
 	'chartOptions' => [
 		'options' => [
 			'seriesBarDistance' => 15
 		],
 		'responsiveOptions' => [
-			[	'screen and (min-width: 641px) and (max-width: 1024px)',
-				[
-					'seriesBarDistance' => 10,
-					'axisX' => [
-						'labelInterpolationFnc' => new JsExpression('function (value) { return value; }'),
-					]
-				]
-			],
 			[	'screen and (max-width: 640px)',
 				[
 					'seriesBarDistance' => 5,
@@ -58,15 +57,44 @@ echo Chartist::widget([
 	],
 	'widgetOptions' => [
 		'type' => 'Bar', // Bar, Line, or Pie, i.e. the chart types supported by Chartist.js
-		'useClass' => 'chartist-chart' // optional parameter, needs to be included in the htmlOptions class string as well if set!
+		'useClass' => 'chartist-chart' // optional parameter, needs to be included in the htmlOptions class string as well if set! Forces the widget to use this class name as reference point for Chartist.js instead of an id
 	],
 	'htmlOptions' => [
 		'class' => 'chartist-chart ct-chart ct-golden-section', // ct-chart for CSS references; size of the charting area needs to be assigned as well
-		// ...
+		//...
 	]
-]);
+]); ?>
 ```
 
-## License
+### Output
+
+```html
+<div id="w0" class="ct-chart ct-golden-section"><!-- <svg /> --></div>
+<!-- ... -->
+<script type="text/javascript">jQuery(document).ready(function () {
+var w0 = new Chartist.Bar("#w0",
+	{
+		"labels": ["Jan","Feb","Mar","Apr","Mai","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
+		"series": [[5,4,3,7,5,10,3,4,8,10,6,8],[3,2,9,5,4,6,4,6,7,8,7,4]]
+	},
+	{
+		"seriesBarDistance":15
+	},
+	[
+		[
+			"screen and (max-width: 640px)",
+			{
+				"seriesBarDistance":5,
+				"axisX": {
+					"labelInterpolationFnc": function (value) { return value[0]; }
+				}
+			}
+		]
+	]);
+});
+</script>
+```
+
+## Liense
 
 **yii2-chartist** is released under the BSD 3-Clause License. See the bundled `LICENSE` for details.
---
+++ examples.php
@@ -0,0 +1,515 @@
+<?php
+use okeanos\chartist\Chartist;
+use View;
+use yii\helpers\Json;
+use yii\web\JsExpression;
+
+/* @var $this View */
+$this->title = 'Examples';
+?>
+    <h1>Line Chart Examples</h1>
+    <h2>Simple Line Chart</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+        'series' => [
+            [12, 9, 7, 8, 5],
+            [2, 1, 3.5, 7, 3],
+            [1, 3, 4, 5, 6]
+        ]
+    ])),
+    'widgetOptions' => [
+        'type' => 'Line',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+    ]
+]);
+?>
+    <h2>Line Scatter Diagram With Responsive Settings</h2>
+<?php $this->registerJs('var times = function(n) {
+  return Array.apply(null, new Array(n));
+};
+
+var lineScatterData = times(52).map(Math.random).reduce(function(data, rnd, index) {
+  data.labels.push(index + 1);
+  data.series.forEach(function(series) {
+    series.push(Math.random() * 100)
+  });
+
+  return data;
+}, {
+  labels: [],
+  series: times(4).map(function() { return new Array() })
+});
+
+var lineScatterOptions = {
+  showLine: false,
+  axisX: {
+    labelInterpolationFnc: function(value, index) {
+      return index % 13 === 0 ? \'W\' + value : null;
+    }
+  }
+};
+
+var lineScatterResponsive = [
+  [\'screen and (min-width: 640px)\', {
+    axisX: {
+      labelInterpolationFnc: function(value, index) {
+        return index % 4 === 0 ? \'W\' + value : null;
+      }
+    }
+  }]
+];', View::POS_READY); ?>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => 'lineScatterData',
+    'chartOptions' => [
+        'options' => 'lineScatterOptions',
+        'responsiveOptions' => 'lineScatterResponsive',
+    ],
+    'widgetOptions' => [
+        'type' => 'Line',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+    ]
+]);
+?>
+    <h2>Line Chart With Tooltips</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => ['1', '2', '3', '4', '5', '6'],
+        'series' => [
+            [
+                'name' => 'Fibonacci sequence',
+                'data' => [1, 2, 3, 5, 8, 13]
+            ],
+            [
+                'name' => 'Golden section',
+                'data' => [1, 1.618, 2.618, 4.236, 6.854, 11.09]
+            ]
+        ]
+    ])),
+    'widgetOptions' => [
+        'type' => 'Line',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+        'id' => 'chartistLineTooltip',
+    ]
+]);
+?>
+<?php $this->registerJs('var $chart = $(\'#chartistLineTooltip\');
+var $toolTip = $chart.append(\'<div class="tooltip"></div>\').find(\'.tooltip\').hide();
+
+$chart.on(\'mouseenter\', \'.ct-point\', function() {
+  var $point = $(this),
+    value = $point.attr(\'ct:value\'),
+    seriesName = $point.parent().attr(\'ct:series-name\');
+  $toolTip.html(seriesName + \'<br>\' + value).show();
+});
+
+$chart.on(\'mouseleave\', \'.ct-point\', function() {
+  $toolTip.hide();
+});
+
+$chart.on(\'mousemove\', function(event) {
+  $toolTip.css({
+    left: (event.offsetX || event.originalEvent.layerX) - $toolTip.width() / 2 - 10,
+    top: (event.offsetY || event.originalEvent.layerY) - $toolTip.height() - 40
+  });
+});', View::POS_READY); ?>
+    <h2>Line Chart With Area</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => [1, 2, 3, 4, 5, 6, 7, 8],
+        'series' => [
+            [5, 9, 7, 8, 5, 3, 5, 4]
+        ]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'low' => 0,
+            'showArea' => true
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Line',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+    ]
+]);
+?>
+    <h2>Bi-Polar Line Chart With Area Only</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => [1, 2, 3, 4, 5, 6, 7, 8],
+        'series' => [
+            [1, 2, 3, 1, -2, 0, 1, 0],
+            [-2, -1, -2, -1, -2.5, -1, -2, -1],
+            [0, 0, 0, 1, 2, 2.5, 2, 1],
+            [2.5, 2, 1, 0.5, 1, 0.5, -1, -2.5]
+        ]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'high' => 3,
+            'low' => -3,
+            'showArea' => true,
+            'showLine' => false,
+            'showPoint' => false,
+            'fullWidth' => true,
+            'axisX' => [
+                'showLabel' => false,
+                'showGrid' => false
+            ]
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Line',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+    ]
+]);
+?>
+    <h2>Using Events To Replace Graphics</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => ['1', '2', '3', '4', '5'],
+        'series' => [
+            [12, 9, 7, 8, 5]
+        ]
+    ])),
+    'widgetOptions' => [
+        'type' => 'Line',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+        'id' => 'chartistLineEvents',
+    ]
+]);
+?>
+<?php $this->registerJs('// Listening for draw events that get emitted by the Chartist chart
+chartistLineEvents.on(\'draw\', function(data) {
+  // If the draw event was triggered from drawing a point on the line chart
+  if(data.type === \'point\') {
+    // We are creating a new path SVG element that draws a triangle around the point coordinates
+    var triangle = new Chartist.Svg(\'path\', {
+      d: [\'M\',
+        data.x,
+        data.y - 15,
+        \'L\',
+        data.x - 15,
+        data.y + 8,
+        \'L\',
+        data.x + 15,
+        data.y + 8,
+        \'z\'].join(\' \'),
+      style: \'fill-opacity: 1\'
+    }, \'ct-area\');
+
+    // With data.element we get the Chartist SVG wrapper and we can replace the original point drawn by Chartist with our newly created triangle
+    data.element.replace(triangle);
+  }
+});', View::POS_READY); ?>
+    <h2>Advanced SMIL Animations</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+        'series' => [
+            [12, 9, 7, 8, 5, 4, 6, 2, 3, 3, 4, 6],
+            [4, 5, 3, 7, 3, 5, 5, 3, 4, 4, 5, 5],
+            [5, 3, 4, 5, 6, 3, 3, 4, 5, 6, 3, 4],
+            [3, 4, 5, 6, 7, 6, 4, 5, 6, 7, 6, 3]
+        ]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'low' => 0,
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Line',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+        'id' => 'chartistLineSMILE',
+    ]
+]);
+?>
+<?php $this->registerJs('// Let\'s put a sequence number aside so we can use it in the event callbacks
+var seq = 0,
+  delays = 80,
+  durations = 500;
+
+// Once the chart is fully created we reset the sequence
+chartistLineSMILE.on(\'created\', function() {
+    seq = 0;
+});
+
+// On each drawn element by Chartist we use the Chartist.Svg API to trigger SMIL animations
+chartistLineSMILE.on(\'draw\', function(data) {
+    seq++;
+
+    if(data.type === \'line\') {
+        // If the drawn element is a line we do a simple opacity fade in. This could also be achieved using CSS3 animations.
+        data.element.animate({
+      opacity: {
+            // The delay when we like to start the animation
+            begin: seq * delays + 1000,
+        // Duration of the animation
+        dur: durations,
+        // The value where the animation should start
+        from: 0,
+        // The value where it should end
+        to: 1
+      }
+    });
+  } else if(data.type === \'label\' && data.axis === \'x\') {
+        data.element.animate({
+      y: {
+            begin: seq * delays,
+        dur: durations,
+        from: data.y + 100,
+        to: data.y,
+        // We can specify an easing function from Chartist.Svg.Easing
+        easing: \'easeOutQuart\'
+      }
+    });
+  } else if(data.type === \'label\' && data.axis === \'y\') {
+        data.element.animate({
+      x: {
+            begin: seq * delays,
+        dur: durations,
+        from: data.x - 100,
+        to: data.x,
+        easing: \'easeOutQuart\'
+      }
+    });
+  } else if(data.type === \'point\') {
+        data.element.animate({
+      x1: {
+            begin: seq * delays,
+        dur: durations,
+        from: data.x - 10,
+        to: data.x,
+        easing: \'easeOutQuart\'
+      },
+      x2: {
+            begin: seq * delays,
+        dur: durations,
+        from: data.x - 10,
+        to: data.x,
+        easing: \'easeOutQuart\'
+      },
+      opacity: {
+            begin: seq * delays,
+        dur: durations,
+        from: 0,
+        to: 1,
+        easing: \'easeOutQuart\'
+      }
+    });
+  } else if(data.type === \'grid\') {
+        // Using data.axis we get x or y which we can use to construct our animation definition objects
+        var pos1Animation = {
+            begin: seq * delays,
+      dur: durations,
+      from: data[data.axis + \'1\'] - 30,
+      to: data[data.axis + \'1\'],
+      easing: \'easeOutQuart\'
+    };
+
+    var pos2Animation = {
+            begin: seq * delays,
+      dur: durations,
+      from: data[data.axis + \'2\'] - 100,
+      to: data[data.axis + \'2\'],
+      easing: \'easeOutQuart\'
+    };
+
+    var animations = {};
+    animations[data.axis + \'1\'] = pos1Animation;
+    animations[data.axis + \'2\'] = pos2Animation;
+    animations[\'opacity\'] = {
+            begin: seq * delays,
+      dur: durations,
+      from: 0,
+      to: 1,
+      easing: \'easeOutQuart\'
+    };
+
+    data.element.animate(animations);
+  }
+});
+
+// For the sake of the example we update the chart every time it\'s created with a delay of 10 seconds
+chartistLineSMILE.on(\'created\', function() {
+    if(window.__exampleAnimateTimeout) {
+        clearTimeout(window.__exampleAnimateTimeout);
+        window.__exampleAnimateTimeout = null;
+    }
+    window.__exampleAnimateTimeout = setTimeout(chart.update.bind(chart), 12000);
+});', View::POS_READY); ?>
+    <h2>SVG Path Animation</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        'series' => [
+            [1, 5, 2, 5, 4, 3],
+            [2, 3, 4, 8, 1, 2],
+            [5, 4, 3, 2, 1, 0.5]
+        ]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'low' => 0,
+            'showArea' => true,
+            'showPoint' => false,
+            'fullWidth' => true
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Line',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+        'id' => 'chartistLineSVGPath',
+    ]
+]);
+?>
+<?php $this->registerJs('chartistLineSVGPath.on(\'draw\', function(data) {
+  if(data.type === \'line\' || data.type === \'area\') {
+    data.element.animate({
+      d: {
+        begin: 2000 * data.index,
+        dur: 2000,
+        from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+        to: data.path.clone().stringify(),
+        easing: Chartist.Svg.Easing.easeOutQuint
+      }
+    });
+  }
+});', View::POS_READY); ?>
+    <h2>Line Interpolation / Smoothing</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => [1, 2, 3, 4, 5],
+        'series' => [
+            [1, 5, 10, 0, 1, 2],
+            [10, 15, 0, 1, 2, 3]
+        ]
+    ])),
+    'chartOptions' => [
+        'options' => [
+// Remove this configuration to see that chart rendered with cardinal spline interpolation
+            // Sometimes, on large jumps in data values, it's better to use simple smoothing.
+            'lineSmooth' => new JsExpression('Chartist.Interpolation.simple(' . Json::encode(['divisor' => 2]) . ')'),
+            'fullWidth' => true,
+            'low' => 0,
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Line',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+    ]
+]);
+?>
+    <h1>Bar Chart Examples</h1>
+    <h2>Bi-Polar Bar Chart</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+        'series' => [
+            [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+        ]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'high' => 10,
+            'low' => -10,
+            'axisX' => [
+                'labelInterpolationFnc' => new JsExpression('function(value, index) {
+        return index % 2 === 0 ? value : null;
+    }')
+            ]
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Bar',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+    ]
+]);
+?>
+    <h2>Overlapping Bars on Mobile</h2>
+<?= Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'Mai',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+        ],
+        'series' => [
+            [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
+            [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
+        ]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'seriesBarDistance' => 15
+        ],
+        'responsiveOptions' => [
+            [
+                'screen and (max-width: 640px)',
+                [
+                    'seriesBarDistance' => 5,
+                    'axisX' => [
+                        'labelInterpolationFnc' => new JsExpression('function (value) { return value[0]; }'),
+                    ]
+                ]
+            ]
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Bar',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+    ]
+]); ?>
