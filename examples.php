b9f79b63ac84a75c198552d2abdbff3031947c7b
Nikolas Grottendieck <github@nikolasgrottendieck.com>
2015-02-28 20:05:33 +0100

    Finished examples

--- examples.php
+++ examples.php
@@ -513,3 +513,377 @@ Chartist::widget([
         'class' => 'ct-chart ct-golden-section',
     ]
 ]); ?>
+    <h2>Add Peak Circles Using The Draw Events</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+        'series' => [
+            [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+        ]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'high' => 10,
+            'low' => -10,
+            'axisX' => [
+                'labelInterpolationFnc' => new JsExpression('function(value, index) { return index % 2 === 0 ? value : null; }')
+            ]
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Bar',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+        'id' => 'chartistBarCircles',
+    ]
+]);
+?>
+<?php $this->registerJs('chartistBarCircles.on(\'draw\', function(data) {
+// If this draw event is of type bar we can use the data to create additional content
+if(data.type === \'bar\') {
+// We use the group element of the current series to append a simple circle with the bar peek coordinates and a circle radius that is depending on the value
+    data.group.append(new Chartist.Svg(\'circle\', {
+        cx: data.x2,
+        cy: data.y2,
+        r: Math.abs(data.value) * 2 + 5
+    }, \'ct-slice\'));
+}
+});', View::POS_READY); ?>
+    <h2>Multi-Line Labels</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => [
+            'First quarter of the year',
+            'Second quarter of the year',
+            'Third quarter of the year',
+            'Fourth quarter of the year'
+        ],
+        'series' => [
+            [60000, 40000, 80000, 70000],
+            [40000, 30000, 70000, 65000],
+            [8000, 3000, 10000, 6000]
+        ]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'seriesBarDistance' => 10,
+            'axisX' => [
+                'offset' => 60
+            ],
+            'axisY' => [
+                'labelInterpolationFnc' => new JsExpression('function(value) { return value + \' CHF\' }'),
+                'scaleMinSpace' => 15
+            ]
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Bar',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+    ]
+]);
+?>
+    <h2>Stacked Bar Chart</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => ['Q1', 'Q2', 'Q3', 'Q4'],
+        'series' => [
+            [800000, 1200000, 1400000, 1300000],
+            [200000, 400000, 500000, 300000],
+            [100000, 200000, 400000, 600000]
+        ]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'stackBars' => true,
+            'axisY' => [
+                'labelInterpolationFnc' => new JsExpression('function(value) { return (value / 1000) + \'k\'; }'),
+                'scaleMinSpace' => 15
+            ]
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Bar',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+        'id' => 'chartistBarStacked'
+    ]
+]);
+?>
+<?php $this->registerJs('chartistBarStacked.on(\'draw\', function(data) {
+if(data.type === \'bar\') {
+    data.element.attr({
+        style: \'stroke-width: 30px\'
+    });
+}
+});', View::POS_READY); ?>
+    <h2>Horizontal Bar Chart</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+        'series' => [
+            [5, 4, 3, 7, 5, 10, 3],
+            [3, 2, 9, 5, 4, 6, 4]
+        ]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'seriesBarDistance' => 10,
+            'reverseData' => true,
+            'horizontalBars' => true,
+            'axisY' => [
+                'offset' => 70
+            ]
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Bar',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+    ]
+]);
+?>
+    <h2>Extreme Responsive Configuration</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => ['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4'],
+        'series' => [
+            [5, 4, 3, 7],
+            [3, 2, 9, 5],
+            [1, 5, 8, 4],
+            [2, 3, 4, 6],
+            [4, 1, 2, 1]
+        ]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'stackBars' => true,
+            'axisX' => [
+                'labelInterpolationFnc' => new JsExpression('function(value) { return value.split(/\s+/).map(function(word) { return word[0]; }).join(\'\'); }')
+            ],
+            'axisY' => [
+                'offset' => 20
+            ]
+        ],
+        'responsiveOptions' => [
+            // Options override for media > 400px
+            [
+                'screen and (min-width: 400px)',
+                [
+                    'reverseData' => true,
+                    'horizontalBars' => true,
+                    'axisX' => [
+                        'labelInterpolationFnc' => new JsExpression('Chartist.noop')
+                    ],
+                    'axisY' => [
+                        'offset' => 60
+                    ]
+                ]
+            ],
+            // Options override for media > 800px
+            [
+                'screen and (min-width: 800px)',
+                [
+                    'stackBars' => false,
+                    'seriesBarDistance' => 10
+                ]
+            ],
+            // Options override for media > 1000px
+            [
+                'screen and (min-width: 1000px)',
+                [
+                    'reverseData' => false,
+                    'horizontalBars' => false,
+                    'seriesBarDistance' => 15
+
+                ]
+            ]
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Bar',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+    ]
+]);
+?>
+    <h1>Pie Chart Examples</h1>
+    <h2>Simple Pie Chart</h2>
+<?php $this->registerJs('var sum = function(a, b) { return a + b };', View::POS_READY); ?>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'series' => [5, 3, 4]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'labelInterpolationFnc' => new JsExpression('function(value) { return Math.round(value / data.series.reduce(sum) * 100) + \'%\'; }')
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Pie',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+    ]
+]);
+?>
+    <h2>Pie Chart With Custom Labels</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => ['Bananas', 'Apples', 'Grapes'],
+        'series' => [20, 15, 40]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'labelInterpolationFnc' => new JsExpression('function(value) { value[0]; }')
+        ],
+        'responsiveOptions' => [
+            [
+                'screen and (min-width: 640px)',
+                [
+                    'chartPadding' => 30,
+                    'labelOffset' => 100,
+                    'labelDirection' => 'explode',
+                    'labelInterpolationFnc' => new JsExpression('function(value) { return value; }')
+                ]
+            ],
+            [
+                'screen and (min-width: 1024px)',
+                [
+                    'labelOffset' => 80,
+                    'chartPadding' => 20
+                ]
+            ]
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Pie',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+    ]
+]);
+?>
+    <h2>Gauge Chart</h2>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'series' => [20, 10, 30, 40]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'donut' => true,
+            'donutWidth' => 60,
+            'startAngle' => 270,
+            'total' => 200,
+            'showLabel' => false
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Pie',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+    ]
+]);
+?>
+    <h1>Using Plugins</h1>
+    <h2>Point Label Plugin</h2>
+<?php $this->registerJsFile('url/to/plugin.js'); ?>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => ['M', 'T', 'W', 'T', 'F'],
+        'series' => [
+            [12, 9, 7, 8, 5]
+        ]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'plugins' => [
+                new JsExpression('Chartist.plugins.ctPointLabels({ textAnchor: \'middle\' })')
+            ]
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Line',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+    ]
+]);
+?>
+    <h2>Sketchy Plugin</h2>
+<?php $this->registerJsFile('url/to/plugin.js'); ?>
+<?=
+Chartist::widget([
+    'tagName' => 'div',
+    'data' => new JsExpression(Json::encode([
+        'labels' => ['Q1', 'Q2', 'Q3', 'Q4'],
+        'series' => [
+            [800000, 1200000, 1400000, 1300000],
+            [200000, 400000, 500000, 300000],
+            [100000, 200000, 400000, 600000]
+        ]
+    ])),
+    'chartOptions' => [
+        'options' => [
+            'plugins' => [
+                new JsExpression('Chartist.plugins.ctSketchy({
+                overrides: {
+                    grid: {
+                      baseFrequency: 0.2,
+                      scale: 5,
+                      numOctaves: 1
+                    },
+                    bar: {
+                      baseFrequency: 0.02,
+                      scale: 10
+                    },
+                    label: false
+                    }
+                })')
+            ],
+            'stackBars' => true,
+            'axisY' => [
+                'labelInterpolationFnc' => new JsExpression('function(value) { return (value / 1000) + \'k\'; }')
+            ]
+        ]
+    ],
+    'widgetOptions' => [
+        'type' => 'Bar',
+    ],
+    'htmlOptions' => [
+        'class' => 'ct-chart ct-golden-section',
+        'id' => 'chartistPluginSketchy'
+    ]
+]);
+?>
+<?php $this->registerJs('chartistPluginSketchy.on(\'draw\', function(data) {
+  if(data.type === \'bar\') {
+    data.element.attr({
+      style: \'stroke-width: 30px\'
+    });
+  }
+});', View::POS_READY); ?>
